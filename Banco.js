563ae5940cc3546b9a08a4d6c452f48c9e26819c
gellanz777 <avp.antonini@gmail.com>
2026-06-15 19:18:06 -0300

    Update Banco.js

--- Banco.js
+++ Banco.js
@@ -1,12 +1,19 @@
 import { Sequelize } from "sequelize";
+import 'dotenv/config';
 
-const banco = new Sequelize('g6_receitas', 'postgres', 'angelo', {
-    host: 'localhost',
-    port: 5432,
-    dialect: 'postgres',
-    define: {
-        timestamps: false,
-        freezeTableName: true
+const banco = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'postgres',
+        define: {
+            timestamps: false,
+            freezeTableName: true
+        }
     }
-});
+);
+
 export default banco;
